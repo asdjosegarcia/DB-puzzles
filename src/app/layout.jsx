2e50593474bf53b411b37a6ac61e9ece7c9bde79
asdjosegarcia <asdjosegarcia@gmail.com>
2023-10-15 02:32:22 -0300

    add api init ok

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] }) //tipo de fuente que saca de google
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div>aca va a estar el navbar</div>
+      {/* <body >{children}</body> */}
     </html>
   )
 }
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+// import Image from 'next/image'
+// import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main >
+    aca va a estar la tabla
+    </main>
+  )
+}
