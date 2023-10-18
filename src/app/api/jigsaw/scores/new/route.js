21a0eb7748602f3fad41624da89fea646b6fd22d
asdjosegarcia <asdjosegarcia@gmail.com>
2023-10-18 03:17:44 -0300

    add realtion but i don't know how to use it for now

--- src/app/api/jigsaw/scores/new/route.js
+++ src/app/api/jigsaw/scores/new/route.js
@@ -4,14 +4,15 @@ import { prisma } from "@/libs/prisma"
 // console.log(prisma)
 
 export async function POST(request){ //post para crear informacion, request la informacion que entra
-    const {user,userId,score,secondsPlayed,movementsNumber}=await request.json()//extramos el user y score de la request que nos enviaron a la api
+    const {user,userId,score,secondsPlayed,movementsNumber,authorId}=await request.json()//extramos el user y score de la request que nos enviaron a la api
     const newScore=await prisma.jigsawScore.create({ //creamos un nuevo objeto con los datos
         data:{
             user,
             userId,
             score,
             secondsPlayed,
-            movementsNumber
+            movementsNumber,
+            authorId:{push:newScore.id}
         }
     })
     return NextResponse.json(newScore)//la api nos va a responder con esto
