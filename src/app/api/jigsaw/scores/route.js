5a4ed7a2bd2403a0a940ce3936e3d1e8885f2ea2
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-23 23:02:38 -0300

    Clean cache

--- src/app/api/jigsaw/scores/route.js
+++ src/app/api/jigsaw/scores/route.js
@@ -3,6 +3,7 @@ import { prisma } from "@/libs/prisma"
 
 
 export async function GET(){
-    const jigsawScores=await prisma.jigsawScore.findMany()
+    const jigsawScores=await prisma.JigsawScore.findMany()
+    console.log(jigsawScores)
     return NextResponse.json(jigsawScores)
 }
