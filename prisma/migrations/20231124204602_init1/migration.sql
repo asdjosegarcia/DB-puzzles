af1ba525c1a6abc2e3980f8f75a9f5a925ee57de
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-25 09:34:48 -0300

    fix api nextjs cache

---
+++ prisma/migrations/20231124204602_init1/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "JigsawScore" (
+    "id" SERIAL NOT NULL,
+    "map" INTEGER NOT NULL DEFAULT 0,
+    "user" TEXT DEFAULT 'unknown',
+    "userId" INTEGER NOT NULL DEFAULT 0,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "secondsPlayed" INTEGER NOT NULL DEFAULT 0,
+    "movementsNumber" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "JigsawScore_pkey" PRIMARY KEY ("id")
+);
