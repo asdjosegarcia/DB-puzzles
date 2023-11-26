4e2417d1b4143ceb4ca301becc91ced56ba9e7dd
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-26 08:53:02 -0300

    add map-number to score-POST

--- src/app/api/jigsaw/scores/new/route.js
+++ src/app/api/jigsaw/scores/new/route.js
@@ -4,14 +4,16 @@ import { prisma } from "@/libs/prisma"
 // console.log(prisma)
 
 export async function POST(request){ //post para crear informacion, request la informacion que entra
-    const {user,userId,score,secondsPlayed,movementsNumber}=await request.json()//extramos el user y score de la request que nos enviaron a la api
+    const {user,userId,score,secondsPlayed,movementsNumber,map}=await request.json()//extramos el user y score de la request que nos enviaron a la api
     const newScore=await prisma.jigsawScore.create({ //creamos un nuevo objeto con los datos
         data:{
             user,
             userId,
             score,
             secondsPlayed,
-            movementsNumber
+            movementsNumber,
+            map,
+
         }
     })
     return NextResponse.json(newScore)//la api nos va a responder con esto
