b40324713897f59cb7887aa97d49c5832d1e3615
asdjosegarcia <asdjosegarcia@gmail.com>
2023-11-29 11:01:19 -0300

    add mapWin to JigsawScore model

---
+++ prisma/migrations/20231129135526_new1/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "JigsawScore" ADD COLUMN     "mapWin" BOOLEAN NOT NULL DEFAULT false;
--- src/app/api/jigsaw/scores/new/route.js
+++ src/app/api/jigsaw/scores/new/route.js
@@ -8,7 +8,7 @@ export async function OPTIONS(request){
 }
 
 export async function POST(request){ //post para crear informacion, request la informacion que entra
-    const {user,userId,score,secondsPlayed,movementsNumber,map}=await request.json()//extramos el user y score de la request que nos enviaron a la api
+    const {user,userId,score,secondsPlayed,movementsNumber,map,mapWin}=await request.json()//extramos el user y score de la request que nos enviaron a la api
     const newScore=await prisma.jigsawScore.create({ //creamos un nuevo objeto con los datos
         data:{
             map,
@@ -17,6 +17,7 @@ export async function POST(request){ //post para crear informacion, request la i
             score,
             secondsPlayed,
             movementsNumber,
+            mapWin,
 
         }
     })
